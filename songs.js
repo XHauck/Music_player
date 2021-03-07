4dea8b5eb7413cbbf4e028c9da4456b97638733d
Himanshu Goyal <53827729+XHauck@users.noreply.github.com>
2021-03-07 15:28:21 +0530

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,22 @@
+const songs = [{
+			name:"1",
+			title:"Lotus Lane",
+			artist:"The Loyallist"
+		},
+		{
+			name:"2",
+			title:"Warrior",
+			artist:"Yoitrax",
+		},
+		{
+			name:"3",
+			title:"sakura",
+			artist:"Roa",
+		},
+		{
+			name:"4",
+			title:"Ronin",
+			artist:"Yoitrax"
+		}
+];
+
